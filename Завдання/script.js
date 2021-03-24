9c788a345fae853c41e03549a4051d812a2e285b
Rytertan <andrij.gorban@gmail.com>
2021-03-24 07:11:16 +0200

    Пройдено 3 практичних уроки включно по цикли

--- Завдання/script.js
+++ Завдання/script.js
@@ -1,29 +1,15 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", "");
+let numberOfFilms;
+
+function start(){
+	numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", "");
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+		numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", "");
+	}
+}
+
+start();
 
 let personalMovieDB = {
 	count : numberOfFilms,
@@ -33,12 +19,47 @@ let personalMovieDB = {
 	privat : false
 };
 
-const first = prompt('Останій фільм який ви подивились?', ''),
-		second = prompt('На скільки ви б його оцінили?', ''),
-		third = prompt('Останій фільм який ви подивились?', ''),
-		fourth = prompt('На скільки ви б його оцінили?', '');
+function rememberMyFilms () {
+	for ( let i = 0; i < 2; i++){
+		const first = prompt('Останій фільм який ви подивились?', ''),
+				second = prompt('На скільки ви б його оцінили?', '');
+		if( first !=null && second !=null && first !='' && second !="" && first.length<50){
+			personalMovieDB.movies[first] = second;
+		}else{
+			i--;
+		}
+	}
+}
 
-personalMovieDB.movies[first] = second;
-personalMovieDB.movies[third] = fourth;
+rememberMyFilms ();
 
-console.log(personalMovieDB);
+function detectPeronalLevel () {
+	if(personalMovieDB.count < 10){
+		console.log('Дуже мало подивились');
+	}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+		console.log('Нормальний такий клядач');
+	}else if(personalMovieDB.count >= 30 ){
+		console.log('Ви Кіноман');
+	}else{
+		console.log('Виникла помилка');
+	}
+}
+
+detectPeronalLevel ();
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB); 
+	}
+}
+
+showMyDB (personalMovieDB.privat);
+
+function writeYourGenres() {
+	for (let i = 1; i<=3; i++){
+		const genre = prompt(`Ваш Улюблений жанр під номером ${i}?`);
+		personalMovieDB.genres[i-1] = genre;
+	}
+}
+
+writeYourGenres ();
